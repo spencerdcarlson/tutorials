b01ce3bed5e2edcb1adab405b4142f7142e6bdac
Spencer Carlson <spencerdcarlson@gmail.com>
2013-03-25 15:07:34 -1000

    pointers instructions

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ## Index ##
 1. pointers/
 	* Pointer.c
-		+ About: Program that demonstrates the use of pointers in C programming
+		+ About: Demonstrates the use of pointers in C programming
 		+ Compile:	$ gcc -o Pointer.o Pointer.c
 		+ Run:		$ ./Pointer.o
 		
--- pointers/Pointer.c
+++ pointers/Pointer.c
@@ -2,27 +2,31 @@
 #include <stdlib.h>
 
 int main(){
+	// Instructions
+	printf("\n  This program demonstrates the characteristics of pointers in the C language.\nAnything surrounded by '' marks is an actual line of code from the program.\n'=>' represents a print out of a variable.\ni.e.  '=> i = 10' represents printf(\"=> i = %%d \", i); when i is equal to 10\n\n ");
+
 	// Declare int a
 	int i = 5;
-	printf("\n	01.) Declare int i = 5;\n");
+	printf("	01.) 'int i = 5;'\n");
 	printf("		=> i = %d\n", i);
-	printf("		=> addr 'i'  = %p\n\n", &i);
+	printf("		=> addr of i:  &i  = %p\n\n", &i);
 
 	// Declare pointer p
 	int *p = &i;
-	printf("	02.) Declare int *p = &i;\n");
-	printf("		=> addr 'p' = %p\n\n", p); 	
+	printf("	02.) 'int *p = &i;'\n");
+	printf("		=> addr of p:  p = %p\n", p); 	
+	printf("		=> *p = %d\n\n", *p);
 	
 	// Change value of a, therfore the value stored at p is also changed. (They are the same item)
 	i = 6;
-	printf("	03.) set i = 6;\n");
+	printf("	03.) 'i = 6;'\n");
 	printf("		=> i = %d\n", i);
 	printf("	Q1.) Does the value of *p change?\n");
 	printf("		=> *p = %d\n\n", *p);
 
 	// Change object stored at *p
 	*p = 10;
-	printf("	04.) set *p = 10;\n");
+	printf("	04.) '*p = 10;'\n");
 	printf("		=> *p = %d\n", *p);
 	printf("	Q2.) Does the value of i change?\n");
 	printf("		=> i = %d\n\n", i);
@@ -36,13 +40,16 @@ int main(){
 	// Change pointer p to some new value
 	// this will actually alter the memory address of p
 	p = (int *)add; 
-	printf("	05.) set p = %lu;\n", add);
+	printf("	05.) 'p = %lu;'\n", add);
 	printf("	Q3.) What is the value of *p now?\n");
 	printf("		=> *p = %d\n", *p);
 	printf("	Q4.) What is the address of p now?\n");
-	printf("		=> addr 'p' = %p or %lu in decimal\n", p,(long)mem); 	
-	printf("	A1.) p = <some int value>;  // Changes the memory address of the pointer.( The new address of 'p' in decimal is the same as 05.) )\n");
-	printf(" 	A2.) *p = <some int value>; // Changes the value of the object stored at the memory address of p along with any other references to that object.\n\n"); 
+	printf("		=> addr of p:  p = %p or %lu in decimal\n\n", p,(long)mem); 	
+	
+	
+	printf(" 	A1.) 'i = <some int value>;'  --  Changes the value of the object, which will also update any the value of any pointer pointing to that object\n");
+	printf(" 	A2.) '*p = <some int value>;' --  Changes the value of the object stored at the memory address of p along with any other references to that object.\n"); 
+	printf("	A3.) 'p = <some int value>;'  --  Changes the memory address of the pointer.(The new address of 'p' in decimal is the same as 05.)\n\n");
 
 	free(mem);	
 	return 0;
